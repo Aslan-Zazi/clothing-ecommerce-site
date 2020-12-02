9ed1bd3ba71af1f5b0ea2ba9d3f95d28fbdb8fd3
Aslan-Zazi <aslan.zazi@gmail.com>
2020-12-02 20:37:52 +0200

    Added shoppage routes and more

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -50,8 +50,8 @@ class DirectoryMenu extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    sections.map(({ id, title, imageUrl, size }) => {
-                        return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    sections.map(({ id, ...otherSectionProps }) => {
+                        return <MenuItem key={id} {...otherSectionProps} />
                     })
                 }
             </div>
