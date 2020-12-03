33e85d804460faf62878f1b7ccdfb7606520a1c8
Aslan-Zazi <aslan.zazi@gmail.com>
2020-12-03 20:15:45 +0200

    Added sigin and signup components and form component

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                SIGN UP
+            </div>
+        );
+    }
+}
+
+export default SignUp;
